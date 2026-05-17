b378967c4de33e8e2d8ca8cdf496860ea2dca9ff
shivanshdhimole <shivanshdhimole@gmail.com>
2026-05-18 02:09:13 +0530

    Create buildingabasiccalculator.cpp

---
+++ buildingabasiccalculator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+    double num1 , num2 ;
+    cout << "enter first number: ";
+    cin >> num1;
+    cout << "enter second number: ";
+    cin >> num2;
+    cout << num1 + num2 ;
+    return 0 ;
+}
