295f4c750b60481761fb20e9b04c7095a97cd963
shivanshdhimole <shivanshdhimole@gmail.com>
2026-05-18 01:00:27 +0530

    Create workingwithnumbers.cpp

---
+++ workingwithnumbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int wnum = 5;
+    int dnum = 3; 
+    wnum+=dnum;
+    cout << wnum << endl;
+    cout << pow(5,3) << endl; 
+    cout << round(56.5) << endl;
+    cout << sqrt(50) << endl;
+    cout << ceil(52.1) << endl;
+    cout << fmax(59 , 100.5) << endl;
+    cout << fmin (0.99 , 0.9) << endl;
+    return 0;
+
+ 
+}
