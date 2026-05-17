14d00a5f7fae788cd8406d1758021bb88701cafb
shivanshdhimole <shivanshdhimole@gmail.com>
2026-05-18 01:00:23 +0530

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int characterAge = 25;
+    string characterName = "John";
+    cout << " there was a man named " << characterName << endl;
+    cout << characterName << " was " << characterAge << " years old" << endl;
+}
