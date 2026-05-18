ab6b81bcd6b8be458e8bc03241ab9e5a84ec9c10
shivanshdhimole <shivanshdhimole@gmail.com>
2026-05-18 10:58:59 +0530

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+void SayHi (string name , int age) {
+ cout << " hello " << name << "your age is " << age << endl;}
+
+ int main() {
+    SayHi ("ansh", 98);
+    SayHi ("mike" , 76);
+    SayHi  ("jumbo", 79);
+    return 0;
+}
