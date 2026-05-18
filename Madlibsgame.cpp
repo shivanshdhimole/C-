6c36f1271f5ccde92b5e109537cafd93e94117c8
shivanshdhimole <shivanshdhimole@gmail.com>
2026-05-18 10:59:02 +0530

    Create Madlibsgame.cpp

---
+++ Madlibsgame.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+    string color , pluralNoun , celebrity ;
+    cout << "enter a color: " ;
+    cin >> color;
+    cout << "enter a plural noun: " ;
+    cin >> pluralNoun;
+    cout << "enter a celebrity: ";
+    cin >> celebrity;
+    cout << "roses are " << color << endl;
+    cout << pluralNoun << "are blue" << endl;
+    cout << "i love " << celebrity << endl;
+    return 0 ;
+
+}
