31d6a759c95d2c539cb74797ce5908bdf800e34c
shivanshdhimole <shivanshdhimole@gmail.com>
2026-05-19 03:10:02 +0530

    Create ifstatement.cpp

---
+++ ifstatement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+    bool isMale = false;
+    bool isTall = true;
+    if (isMale) {
+        cout << "yes you are a male" ;
+        } else {
+            cout << "you are not a male" ;
+        }
+        return 0;
+
+}
